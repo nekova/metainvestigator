2fae6f24afb009ba4f29ba0233647c5a2c11c749
nekova <nekova07@gmail.com>
2015-10-30 06:15:38 +0900

    Return struct

--- lib/metainspector.ex
+++ lib/metainspector.ex
@@ -1,11 +1,11 @@
 defmodule MetaInspector do
-  defstruct status: nil, title: nil
-  @type t :: %__MODULE__{status: integer, title: String.t}
+  defstruct status: nil, title: nil, best_title: nil
+  @type t :: %__MODULE__{status: integer, title: String.t, best_title: String.t}
 
   def new(url) do
     document = HTTPoison.get!(url)
     body = document.body
-    %MetaInspector{status: status(document), title: title(body)}
+    %MetaInspector{status: status(document), title: title(body), best_title: best_title(body)}
   end
 
   def status(document) do
@@ -13,7 +13,32 @@ defmodule MetaInspector do
   end
 
   def title(body) do
-    Floki.find(body, "head title")
+    body
+    |> Floki.find("head title")
     |> Floki.text
+  end
+
+  def og_title(body) do
+    meta_tag_by(body, "title")
+  end
+
+  def best_title(body) do
+    og_title = og_title(body)
+    title    = title(body)
+    case og_title > title do
+      true  -> og_title
+      false -> title
+    end
+  end
+
+  # attribute = "title", "description", "image", "url", "type"
+  @spec meta_tag_by(String.t, String.t) :: [String.t]
+  defp meta_tag_by(body, attribute) do
+    Floki.find(body, "[property=\"og:#{attribute}\"]")
+    |> Floki.attribute("content")
+  end
+
+  defmacro exist?(attribute) do
+    quote do: unquote(String.length(attribute)) > 0
   end
 end
