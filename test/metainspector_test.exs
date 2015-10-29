1acb4feef02f2beb3ca3d9ef294184adcd661a06
nekova <nekova07@gmail.com>
2015-10-30 08:20:09 +0900

    Add testcase for title

--- test/metainspector_test.exs
+++ test/metainspector_test.exs
@@ -1,7 +1,25 @@
 defmodule MetaInspectorTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  @html """
+  <html>
+  <head>
+  <title>MetaInspector</title>
+  <meta content="MetaInspector in Test" property="og:title">
+  </head>
+  <body></body>
+  </html>
+  """
+
+  test "title" do
+    assert "MetaInspector" == MetaInspector.title(@html)
+  end
+
+  test "og_title" do
+    assert ["MetaInspector in Test"] == MetaInspector.og_title(@html)
+  end
+
+  test "best_title" do
+    assert "MetaInspector in Test" == MetaInspector.best_title(@html)
   end
 end
