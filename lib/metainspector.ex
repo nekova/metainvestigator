f13090ea357633e0c9edf1929b99b1cab1c7df8b
nekova <nekova07@gmail.com>
2015-10-31 19:16:19 +0900

    Fix type of metadata

--- lib/metainspector.ex
+++ lib/metainspector.ex
@@ -1,6 +1,6 @@
 defmodule MetaInspector.Meta do
-  defstruct og_title: nil, og_type: nil, og_url: nil, og_image: nil
-  @type t :: %__MODULE__{og_title: char_list, og_type: char_list, og_url: char_list, og_image: char_list}
+  defstruct og_title: [], og_type: [], og_url: [], og_image: []
+  @type t :: %__MODULE__{og_title: list, og_type: list, og_url: list, og_image: list}
 end
 
 defmodule MetaInspector do
